46e72e11a7c3b92f7c8839ec56d492a892e2c139
Niel Lebeck <niel.lebeck@gmail.com>
2017-04-02 10:01:21 -0700

    Add keyboard mode and clean up names

--- XboxControllerRemote/XboxControllerRemote/MainForm.cs
+++ XboxControllerRemote/XboxControllerRemote/MainForm.cs
@@ -11,8 +11,10 @@ namespace XboxControllerRemote
 {
     public partial class MainForm : Form
     {
-        private const string browserFileName = "IExplore.exe";
-        private const string netflixURL = "https://www.netflix.com";
+        private const string BROWSER_FILE_NAME = "IExplore.exe";
+        private const string NETFLIX_URL = "https://www.netflix.com";
+        private const int BUTTON_PRESS_SLEEP_MS = 100;
+        private const int POLLING_INTERVAL_MS = 10;
 
         [DllImport("User32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hwnd);
@@ -22,6 +24,7 @@ namespace XboxControllerRemote
         private XInputState prevState;
         private System.Timers.Timer timer;
         private Process browserProcess = null;
+        private bool inKeyboardMode = false;
 
         public MainForm()
         {
@@ -29,12 +32,12 @@ namespace XboxControllerRemote
 
             this.Cursor = new Cursor(Cursor.Current.Handle);
 
-            timer = new System.Timers.Timer(10);
+            timer = new System.Timers.Timer(POLLING_INTERVAL_MS);
             timer.Elapsed += (sender, e) => Invoke(new detectInputDelegate(DetectInput));
             timer.Start();
         }
 
-        private bool buttonPressed(XInputState state, XInputState prevState, ushort buttonMask)
+        private bool ButtonPressed(XInputState state, XInputState prevState, ushort buttonMask)
         {
             return((state.Gamepad.wButtons & buttonMask) > 0 && (prevState.Gamepad.wButtons & buttonMask) == 0) ;
         }
@@ -43,37 +46,52 @@ namespace XboxControllerRemote
         {
             XInputState state = XInputState.XInputGetStateWrapper(0);
 
-            int offsetX = 0;
-            int offsetY = 0;
-            if (Math.Abs((int)state.Gamepad.sThumbLX) > XInputConstants.GAMEPAD_LEFT_THUMB_DEADZONE)
+            if (inKeyboardMode)
             {
-                offsetX = (int)(state.Gamepad.sThumbLX / 32767.0 * 25.0);
-            }
-            if (Math.Abs((int)state.Gamepad.sThumbLY) > XInputConstants.GAMEPAD_LEFT_THUMB_DEADZONE)
-            {
-                offsetY = (int)(state.Gamepad.sThumbLY / 32767.0 * 25.0);
-            }
-            Cursor.Position = new Point(Cursor.Position.X + offsetX, Cursor.Position.Y - offsetY);
-
-            if (buttonPressed(state, prevState, XInputConstants.GAMEPAD_A))
-            {
-                MouseEventWrapper.MouseEvent(MouseEventWrapper.MOUSEEVENTTF_LEFTDOWN, 0, 0, 0, 0);
-                MouseEventWrapper.MouseEvent(MouseEventWrapper.MOUSEEVENTTF_LEFTUP, 0, 0, 0, 0);
-            }
-
-            if (buttonPressed(state, prevState, XInputConstants.GAMEPAD_START))
-            {
-                if (browserProcess == null)
+                if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_BACK))
                 {
-                    browserProcess = Process.Start(browserFileName, netflixURL);
+                    inKeyboardMode = false;
+                    SetForegroundWindow(browserProcess.MainWindowHandle);
+                }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_A))
+                {
+                    SetForegroundWindow(browserProcess.MainWindowHandle);
+                    Thread.Sleep(BUTTON_PRESS_SLEEP_MS);
+                    SendKeys.Send("A");
+                    Thread.Sleep(BUTTON_PRESS_SLEEP_MS);
+                    SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
                 }
             }
-
-            if (buttonPressed(state, prevState, XInputConstants.GAMEPAD_BACK))
+            else
             {
-                if (browserProcess != null)
+                int offsetX = 0;
+                int offsetY = 0;
+                if (Math.Abs((int)state.Gamepad.sThumbLX) > XInputConstants.GAMEPAD_LEFT_THUMB_DEADZONE)
                 {
-                    bool result = SetForegroundWindow(browserProcess.MainWindowHandle);
+                    offsetX = (int)(state.Gamepad.sThumbLX / 32767.0 * 25.0);
+                }
+                if (Math.Abs((int)state.Gamepad.sThumbLY) > XInputConstants.GAMEPAD_LEFT_THUMB_DEADZONE)
+                {
+                    offsetY = (int)(state.Gamepad.sThumbLY / 32767.0 * 25.0);
+                }
+                Cursor.Position = new Point(Cursor.Position.X + offsetX, Cursor.Position.Y - offsetY);
+
+                if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_A))
+                {
+                    MouseEventWrapper.MouseEvent(MouseEventWrapper.MOUSEEVENTTF_LEFTDOWN, 0, 0, 0, 0);
+                    MouseEventWrapper.MouseEvent(MouseEventWrapper.MOUSEEVENTTF_LEFTUP, 0, 0, 0, 0);
+                }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_START))
+                {
+                    if (browserProcess == null)
+                    {
+                        browserProcess = Process.Start(BROWSER_FILE_NAME, NETFLIX_URL);
+                    }
+                }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_BACK))
+                {
+                    inKeyboardMode = true;
+                    SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
                 }
             }
 
