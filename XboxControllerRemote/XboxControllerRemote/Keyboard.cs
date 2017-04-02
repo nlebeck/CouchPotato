49c96b4e1e61c70e736e575baf1b421bb362aff0
Niel Lebeck <niel.lebeck@gmail.com>
2017-04-02 11:41:54 -0700

    Add keyboard drawing and selected key detection

---
+++ XboxControllerRemote/XboxControllerRemote/Keyboard.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace XboxControllerRemote
+{
+    public class Keyboard
+    {
+        public enum Direction { Left, Right, Up, Down };
+
+        private static string FONT = "Arial";
+
+        private string[][] keys;
+        private int width;
+        private int height;
+
+        private int selectedCol;
+        private int selectedRow;
+
+        public Keyboard(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            keys = new string[3][];
+            keys[0] = new string[] { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"};
+            keys[1] = new string[] { "A", "S", "D", "F", "G", "H", "J", "K", "L", "{ENTER}" };
+            keys[2] = new string[] { "Z", "X", "C", "V", "B", "N", "M" };
+
+            selectedCol = 0;
+            selectedRow = 0;
+        }
+
+        private int GetMaxRowLength()
+        {
+            int maxLength = 0;
+            foreach(string[] row in keys)
+            {
+                if (row.Length > maxLength)
+                {
+                    maxLength = row.Length;
+                }
+            }
+            return maxLength;
+        }
+
+        private int GetNumRows()
+        {
+            return keys.Length;
+        }
+
+        private int GetKeyWidth()
+        {
+            return width / GetMaxRowLength();
+        }
+        
+        private int GetKeyHeight()
+        {
+            return height / GetNumRows();
+        }
+
+        public void DrawKeyboard(Graphics graphics)
+        {
+            graphics.Clear(Color.Gray);
+
+            for (int row = 0; row < keys.Length; row++)
+            {
+                for (int col = 0; col < keys[row].Length; col++)
+                {
+                    string key = keys[row][col];
+
+                    int x = GetKeyWidth() * col;
+                    int y = GetKeyHeight() * row;
+
+                    Rectangle rect = new Rectangle(x, y, GetKeyWidth(), GetKeyHeight());
+
+                    int fontSize = GetKeyWidth() / 2;
+                    int textX = x;
+                    int textY = y + GetKeyHeight() / 4;
+                    if (key.Length > 1)
+                    {
+                        fontSize = GetKeyWidth() / 7;
+                        textY = y + GetKeyHeight() / 2;
+                    }
+                    Font font = new Font(FONT, fontSize);
+
+                    if (col == selectedCol && row == selectedRow)
+                    {
+                        graphics.FillRectangle(Brushes.Black, rect);
+                        graphics.DrawString(key, font, Brushes.White, textX, textY);
+                    }
+                    else
+                    {
+                        graphics.DrawRectangle(Pens.Black, rect);
+                        graphics.DrawString(key, font, Brushes.Black, textX, textY);
+                    }
+
+                }
+            }
+        }
+
+        public void MoveCursor(Direction direction)
+        {
+            int modifiedCol = selectedCol;
+            int modifiedRow = selectedRow;
+
+            switch (direction)
+            {
+                case Direction.Left:
+                    modifiedCol -= 1;
+                    break;
+                case Direction.Right:
+                    modifiedCol += 1;
+                    break;
+                case Direction.Up:
+                    modifiedRow -= 1;
+                    break;
+                case Direction.Down:
+                    modifiedRow += 1;
+                    break;
+            }
+
+            Debug.WriteLine("Moving: {0}, {1}", modifiedCol, modifiedRow);
+
+            if (modifiedRow < 0)
+            {
+                modifiedRow = 0;
+            }
+            if (modifiedRow >= GetNumRows())
+            {
+                modifiedRow = GetNumRows() - 1;
+            }
+            if (modifiedCol < 0)
+            {
+                modifiedCol = 0;
+            }
+            if (modifiedCol >= keys[modifiedRow].Length)
+            {
+                modifiedCol = keys[modifiedRow].Length - 1;
+            }
+
+            selectedCol = modifiedCol;
+            selectedRow = modifiedRow;
+
+            Debug.WriteLine("After: {0}, {1}", selectedCol, selectedRow);
+        }
+
+        public string GetSelectedKey()
+        {
+            return keys[selectedRow][selectedCol];
+        }
+    }
+}
--- XboxControllerRemote/XboxControllerRemote/MainForm.cs
+++ XboxControllerRemote/XboxControllerRemote/MainForm.cs
@@ -13,8 +13,13 @@ namespace XboxControllerRemote
     {
         private const string BROWSER_FILE_NAME = "IExplore.exe";
         private const string NETFLIX_URL = "https://www.netflix.com";
+
         private const int BUTTON_PRESS_SLEEP_MS = 100;
         private const int POLLING_INTERVAL_MS = 10;
+
+        private const int HRES = 1920;
+        private const double KEYBOARD_WIDTH_SCALE = 0.5;
+        private const double KEYBOARD_ASPECT_RATIO = 16.0 / 9.0;
 
         [DllImport("User32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hwnd);
@@ -24,6 +29,8 @@ namespace XboxControllerRemote
         private XInputState prevState;
         private System.Timers.Timer timer;
         private Process browserProcess = null;
+        private Keyboard keyboard;
+
         private bool inKeyboardMode = false;
 
         public MainForm()
@@ -35,6 +42,11 @@ namespace XboxControllerRemote
             timer = new System.Timers.Timer(POLLING_INTERVAL_MS);
             timer.Elapsed += (sender, e) => Invoke(new detectInputDelegate(DetectInput));
             timer.Start();
+
+            Width = (int)(HRES * KEYBOARD_WIDTH_SCALE);
+            Height = (int)(Width / KEYBOARD_ASPECT_RATIO);
+
+            keyboard = new Keyboard(Width, Height);
         }
 
         private bool ButtonPressed(XInputState state, XInputState prevState, ushort buttonMask)
@@ -48,16 +60,36 @@ namespace XboxControllerRemote
 
             if (inKeyboardMode)
             {
+                Graphics formGraphics = CreateGraphics();
+                keyboard.DrawKeyboard(formGraphics);
+                formGraphics.Dispose();
+
                 if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_BACK))
                 {
                     inKeyboardMode = false;
                     SetForegroundWindow(browserProcess.MainWindowHandle);
                 }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_DPAD_LEFT))
+                {
+                    keyboard.MoveCursor(Keyboard.Direction.Left);
+                }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_DPAD_RIGHT))
+                {
+                    keyboard.MoveCursor(Keyboard.Direction.Right);
+                }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_DPAD_UP))
+                {
+                    keyboard.MoveCursor(Keyboard.Direction.Up);
+                }
+                else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_DPAD_DOWN))
+                {
+                    keyboard.MoveCursor(Keyboard.Direction.Down);
+                }
                 else if (ButtonPressed(state, prevState, XInputConstants.GAMEPAD_A))
                 {
                     SetForegroundWindow(browserProcess.MainWindowHandle);
                     Thread.Sleep(BUTTON_PRESS_SLEEP_MS);
-                    SendKeys.Send("A");
+                    SendKeys.Send(keyboard.GetSelectedKey());
                     Thread.Sleep(BUTTON_PRESS_SLEEP_MS);
                     SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
                 }
