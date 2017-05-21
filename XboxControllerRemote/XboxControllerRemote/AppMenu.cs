e69bf9949cacb34d15501c4943a3fa8cab719142
Niel Lebeck <niel.lebeck@gmail.com>
2017-05-20 17:13:10 -0700

    Move config file parsing to its own class

--- XboxControllerRemote/XboxControllerRemote/AppMenu.cs
+++ XboxControllerRemote/XboxControllerRemote/AppMenu.cs
@@ -13,7 +13,14 @@ namespace XboxControllerRemote
 
         public AppMenu(MainForm form, int width, int height) : base(form, width, height)
         {
-            menuItems = LoadMenuItems();
+            try
+            {
+                menuItems = ConfigFileParser.LoadMenuItems();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                mainForm.ExitWithMessage("Config file not found.");
+            }
             menuItems.Add(new MouseEmulatorItem());
             menuItems.Add(new ShutdownItem());
             menuItems.Add(new QuitItem());
@@ -65,117 +72,6 @@ namespace XboxControllerRemote
         {
             AppMenuItem selectedItem = menuItems[selectedIndex];
             mainForm.StartApp(selectedItem);
-        }
-
-        public List<AppMenuItem> LoadMenuItems()
-        {
-            List<AppMenuItem> menuItems = new List<AppMenuItem>();
-            XmlReader reader = null;
-            try
-            {
-                reader = XmlReader.Create("Config.xml");
-            }
-            catch(System.IO.FileNotFoundException)
-            {
-                mainForm.ExitWithMessage("Config file not found.");
-            }
-            reader.ReadToNextSibling("configuration");
-            reader.ReadToDescendant("menuItems");
-            while (reader.Read())
-            {
-                if (reader.NodeType == XmlNodeType.Element)
-                {
-                    if (reader.Name.Equals("website"))
-                    {
-                        menuItems.Add(ParseWebsiteItem(reader.ReadSubtree()));
-                    }
-                    else if (reader.Name.Equals("program"))
-                    {
-                        menuItems.Add(ParseProgramItem(reader.ReadSubtree()));
-                    }
-                    else if (reader.Name.Equals("controllerProgram"))
-                    {
-                        menuItems.Add(ParseControllerProgramItem(reader.ReadSubtree()));
-                    }
-                }
-            }
-            return menuItems;
-        }
-
-        public WebsiteItem ParseWebsiteItem(XmlReader reader)
-        {
-            string name = null;
-            string url = null;
-            while (reader.Read())
-            {
-                if (reader.NodeType == XmlNodeType.Element)
-                {
-                    if (reader.Name.Equals("name"))
-                    {
-                        name = reader.ReadElementContentAsString();
-                    }
-                    else if (reader.Name.Equals("url"))
-                    {
-                        url = reader.ReadElementContentAsString();
-                    }
-                }
-            }
-            return new WebsiteItem(name, url);
-        }
-
-        public ProgramItem ParseProgramItem(XmlReader reader)
-        {
-            Dictionary<string, string> members = ParseProgramItemHelper(reader);
-            return new ProgramItem(members["name"], members["processName"], members["processPath"], members["args"], members["appStartedArgs"]);
-        }
-
-        public ControllerProgramItem ParseControllerProgramItem(XmlReader reader)
-        {
-            Dictionary<string, string> members = ParseProgramItemHelper(reader);
-            return new ControllerProgramItem(members["name"], members["processName"], members["processPath"], members["args"], members["appStartedArgs"]);
-        }
-
-        public Dictionary<string, string> ParseProgramItemHelper(XmlReader reader)
-        {
-            string name = null;
-            string processName = null;
-            string processPath = null;
-            string args = null;
-            string appStartedArgs = null;
-            while (reader.Read())
-            {
-                if (reader.NodeType == XmlNodeType.Element)
-                {
-                    if (reader.Name.Equals("name"))
-                    {
-                        name = reader.ReadElementContentAsString();
-                    }
-                    else if (reader.Name.Equals("processName"))
-                    {
-                        processName = reader.ReadElementContentAsString();
-                    }
-                    else if (reader.Name.Equals("processPath"))
-                    {
-                        processPath = reader.ReadElementContentAsString();
-                    }
-                    else if (reader.Name.Equals("args"))
-                    {
-                        args = reader.ReadElementContentAsString();
-                    }
-                    else if (reader.Name.Equals("appStartedArgs"))
-                    {
-                        appStartedArgs = reader.ReadElementContentAsString();
-                    }
-                }
-            }
-            return new Dictionary<string, string>()
-            {
-                { "name", name },
-                { "processName", processName },
-                { "processPath", processPath },
-                { "args", args },
-                { "appStartedArgs", appStartedArgs }
-            };
         }
     }
 }
---
+++ XboxControllerRemote/XboxControllerRemote/ConfigFileParser.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using XboxControllerRemote.AppMenuItems;
+
+namespace XboxControllerRemote
+{
+    public static class ConfigFileParser
+    {
+
+        public static List<AppMenuItem> LoadMenuItems()
+        {
+            List<AppMenuItem> menuItems = new List<AppMenuItem>();
+            XmlReader reader = XmlReader.Create("Config.xml");
+            reader.ReadToNextSibling("configuration");
+            reader.ReadToDescendant("menuItems");
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name.Equals("website"))
+                    {
+                        menuItems.Add(ParseWebsiteItem(reader.ReadSubtree()));
+                    }
+                    else if (reader.Name.Equals("program"))
+                    {
+                        menuItems.Add(ParseProgramItem(reader.ReadSubtree()));
+                    }
+                    else if (reader.Name.Equals("controllerProgram"))
+                    {
+                        menuItems.Add(ParseControllerProgramItem(reader.ReadSubtree()));
+                    }
+                }
+            }
+            return menuItems;
+        }
+
+        public static WebsiteItem ParseWebsiteItem(XmlReader reader)
+        {
+            string name = null;
+            string url = null;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name.Equals("name"))
+                    {
+                        name = reader.ReadElementContentAsString();
+                    }
+                    else if (reader.Name.Equals("url"))
+                    {
+                        url = reader.ReadElementContentAsString();
+                    }
+                }
+            }
+            return new WebsiteItem(name, url);
+        }
+
+        public static ProgramItem ParseProgramItem(XmlReader reader)
+        {
+            Dictionary<string, string> members = ParseProgramItemHelper(reader);
+            return new ProgramItem(members["name"], members["processName"], members["processPath"], members["args"], members["appStartedArgs"]);
+        }
+
+        public static ControllerProgramItem ParseControllerProgramItem(XmlReader reader)
+        {
+            Dictionary<string, string> members = ParseProgramItemHelper(reader);
+            return new ControllerProgramItem(members["name"], members["processName"], members["processPath"], members["args"], members["appStartedArgs"]);
+        }
+
+        public static Dictionary<string, string> ParseProgramItemHelper(XmlReader reader)
+        {
+            string name = null;
+            string processName = null;
+            string processPath = null;
+            string args = null;
+            string appStartedArgs = null;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name.Equals("name"))
+                    {
+                        name = reader.ReadElementContentAsString();
+                    }
+                    else if (reader.Name.Equals("processName"))
+                    {
+                        processName = reader.ReadElementContentAsString();
+                    }
+                    else if (reader.Name.Equals("processPath"))
+                    {
+                        processPath = reader.ReadElementContentAsString();
+                    }
+                    else if (reader.Name.Equals("args"))
+                    {
+                        args = reader.ReadElementContentAsString();
+                    }
+                    else if (reader.Name.Equals("appStartedArgs"))
+                    {
+                        appStartedArgs = reader.ReadElementContentAsString();
+                    }
+                }
+            }
+            return new Dictionary<string, string>()
+            {
+                { "name", name },
+                { "processName", processName },
+                { "processPath", processPath },
+                { "args", args },
+                { "appStartedArgs", appStartedArgs }
+            };
+        }
+    }
+}
